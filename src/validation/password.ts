33f15427853d6fe22ef9133d94502cfa4bb2d569
arvid-e <arvid.edvinsson@gmail.com>
2025-09-12 12:08:30 +0900

    Add password validator

---
+++ src/validation/password.ts
@@ -0,0 +1,35 @@
+export function isValidPassword(password: string): boolean {
+    if (containsLowerCaseLetter(password) &&
+        containsUppercaseLetter(password) &&
+        containsSpecialCharacter(password)) {
+
+        return true;
+    } 
+
+    else {
+        return false;
+    }
+}
+
+function containsUppercaseLetter(password: string): boolean {
+    const UPPER_CASE_REGEX = /[A-Z]/;
+    const hasUppercaseLetter = UPPER_CASE_REGEX.test(password);
+
+    return hasUppercaseLetter;
+
+}
+
+function containsLowerCaseLetter(password: string): boolean {
+    const LOWER_CASE_REGEX = /[a-z]/;
+    const hasLowerCaseLetter = LOWER_CASE_REGEX.test(password);
+
+    return hasLowerCaseLetter;
+}
+
+function containsSpecialCharacter(password: string): boolean {
+    const SPECIAL_CHARACTER_REGEX = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    const hasSpecialCharacter = SPECIAL_CHARACTER_REGEX.test(password);
+
+    return hasSpecialCharacter;
+}
+
