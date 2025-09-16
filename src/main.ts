5b291362a3ea80bd196e06e7c7f5c105b1dfa368
arvid-e <arvid.edvinsson@gmail.com>
2025-09-17 08:58:34 +0900

    Export validators from the application entry point

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
-import { isValidEmail } from './validation/email.js';
-import { isValidDate } from './validation/date.js';
-import { isValidPassword } from './validation/password.js';
-
-const email = 'kalle.kollen@hotmail.com';
-const longDate = 'June 1, 2025';
-const password = 'aBbabababa#';
-
-console.log('Valid email: ', isValidEmail(email));
-console.log('Valid date: ', isValidDate(longDate));
-console.log('Valid password:', isValidPassword(password));
+export * from './validation/date.js';
+export * from './validation/email.js';
+export * from './validation/password.js';
