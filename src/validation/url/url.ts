dbc07702cbe1086165fd1296739a320895a95e11
arvid-e <arvid.edvinsson@gmail.com>
2025-09-19 12:40:28 +0900

    Make a function for constructing the url regex

--- src/validation/url/url.ts
+++ src/validation/url/url.ts
@@ -1,44 +1,49 @@
+interface UrlsParts {
+  protocol: string;
+  domainName: string;
+  path?: string;
+  query?: string;
+  fragment?: string;
+}
+
 export class UrlValidator {
   constructor() {}
 
   public isValidUrl(url: string): boolean {
-    const domainName = this.getDomainName(url);
 
-    if (!domainName) {
-      return false;
-    }
-
-    const protocolIsValid = this.hasValidProtocol(url);
-    const domainNameIsValid = this.hasValidHostNameLength(domainName);
-
-    return protocolIsValid && domainNameIsValid;
   }
 
-  private getDomainName(url: string): string | null {
-    const HOSTNAME_PARTS_REGEX = [
+  private contructUrlRegex(): RegExp {
+    const protocolRegexStrings = ['^(?<protocol>https?:\/\/)']; // Starts with http:// or https://
+
+    const hostnameRegexStrings = [
       '[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?', //  Hostname starts and ends with a letter or number
       '(?:\\.[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*', // Optional labels must be separated by a period
       '\\.[A-Za-z]{2,63}', // Ending top level domain must be 2-63 letters
     ];
 
-    const HOSTNAME_REGEX: RegExp = new RegExp(HOSTNAME_PARTS_REGEX.join(''));
+    const optionalPortRegexStrings = ['(?::\d{1,5})?'];
 
-    const hostnameMatch = url.match(HOSTNAME_REGEX);
+    const optionalPathRegexStrings = ['(?:\/[^?#]*)?'];
 
-    if (!hostnameMatch) {
-      return null;
-    }
+    const optionalQueryRegexStrings = ['(?:\\?[^#]*)?'];
 
-    const hostname = hostnameMatch[0];
+    const optionalFragmentRegexStrings = ['(?:#.*)?$'];
 
-    return hostname;
-  }
+    const combinedUrlRegexStrings = [
+      protocolRegexStrings,
+      hostnameRegexStrings,
+      optionalPortRegexStrings,
+      optionalPathRegexStrings,
+      optionalQueryRegexStrings,
+      optionalFragmentRegexStrings,
+    ];
 
-  private hasValidProtocol(url: string): boolean {
-    const PROTOCOL_REGEX: RegExp = /^(?<protocol>https?:\/\/)/;
-    const hasValidProtocol = PROTOCOL_REGEX.test(url);
+    const urlRegex: RegExp = new RegExp(
+      combinedUrlRegexStrings.flat().join(''),
+    );
 
-    return hasValidProtocol;
+    return urlRegex;
   }
 
   private hasValidHostNameLength(hostname: string): boolean {
