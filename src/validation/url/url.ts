576e2ad87707d2f79b78acd04fae8479410f4795
arvid-e <arvid.edvinsson@gmail.com>
2025-09-19 10:42:03 +0900

    Change url validator into a class instead of seperate functions

--- src/validation/url/url.ts
+++ src/validation/url/url.ts
@@ -1,5 +1,51 @@
-import { urlHasValidHostname, urlHasValidProtocol } from './url-helpers.js';
+export class UrlValidator {
+  constructor() {}
 
-export function isValidURL(url: string): boolean {
-  return urlHasValidProtocol(url) && urlHasValidHostname(url);
+  public isValidUrl(url: string): boolean {
+    const domainName = this.getDomainName(url);
+
+    if (!domainName) {
+      return false;
+    }
+
+    const protocolIsValid = this.hasValidProtocol(url);
+    const domainNameIsValid = this.hasValidHostNameLength(domainName);
+
+    return protocolIsValid && domainNameIsValid;
+  }
+
+  private getDomainName(url: string): string | null {
+    const HOSTNAME_PARTS_REGEX = [
+      '[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?', //  Hostname starts and ends with a letter or number
+      '(?:\\.[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*', // Optional labels must be separated by a period
+      '\\.[A-Za-z]{2,63}', // Ending top level domain must be 2-63 letters
+    ];
+
+    const HOSTNAME_REGEX: RegExp = new RegExp(HOSTNAME_PARTS_REGEX.join(''));
+
+    const hostnameMatch = url.match(HOSTNAME_REGEX);
+
+    if (!hostnameMatch) {
+      return null;
+    }
+
+    const hostname = hostnameMatch[0];
+
+    return hostname;
+  }
+
+  private hasValidProtocol(url: string): boolean {
+    const PROTOCOL_REGEX: RegExp = /(?<protocol>https?:\/\/)/;
+    const hasValidProtocol = PROTOCOL_REGEX.test(url);
+
+    return hasValidProtocol;
+  }
+
+  private hasValidHostNameLength(hostname: string): boolean {
+    if (hostname.length > 255) {
+      return false;
+    }
+
+    return true;
+  }
 }
