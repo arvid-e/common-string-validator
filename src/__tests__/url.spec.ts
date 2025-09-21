8ed9e8292603cd2b3928038872cf0f4a38245d3f
arvid-e <arvid.edvinsson@gmail.com>
2025-09-21 14:38:41 +0900

    Write tests for the url validator

--- src/__tests__/url.spec.ts
+++ src/__tests__/url.spec.ts
@@ -8,4 +8,29 @@ describe('isValidUrl()', () => {
     const result = urlValidator.isValidUrl(validUrl);
     expect(result).toBe(true);
   });
+
+  it('should return false on consecutive periods in hostname', () => {
+    const invalidUrl = 'https://mysite..more.com';
+    const result = urlValidator.isValidUrl(invalidUrl);
+    expect(result).toBe(false);
+  });
+
+  it('should return false on too long hosname', () => {
+    const longString = 'a'.repeat(256);
+    const invalidUrl = `https://${longString}.com`;
+    const result = urlValidator.isValidUrl(invalidUrl);
+    expect(result).toBe(false);
+  });
+
+  it('should return false on invalid port', () => {
+    const invalidUrl = 'https://mysite.more.com:9999999';
+    const result = urlValidator.isValidUrl(invalidUrl);
+    expect(result).toBe(false);
+  });
+
+  it('should return false on invalid protocol', () => {
+    const invalidUrl = 'hps://mysite.more.com';
+    const result = urlValidator.isValidUrl(invalidUrl);
+    expect(result).toBe(false);
+  });
 });
