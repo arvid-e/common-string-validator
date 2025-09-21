463ea03f0661983c38010fea8ff9bf2e9266d901
arvid-e <arvid.edvinsson@gmail.com>
2025-09-21 15:47:42 +0900

    Write JSDoc comments for the url validator

--- src/validation/url/url.ts
+++ src/validation/url/url.ts
@@ -1,3 +1,6 @@
+/**
+ * Class used to check if a URL string is valid.
+ */
 export class UrlValidator {
   urlRegex: RegExp;
 
@@ -5,12 +8,23 @@ export class UrlValidator {
     this.urlRegex = this.contructUrlRegex();
   }
 
+  /**
+   * Validates a URL string.
+   *
+   * @param {string} url - URL string to validate.
+   * @returns {boolean} - True if valid, false if not.
+   */
   public isValidUrl(url: string): boolean {
     const urlIsValid = this.extractAndValidateUrl(url, this.urlRegex);
 
     return urlIsValid;
   }
 
+  /**
+   * Constructs and returns regular expression used to validate a URL string.
+   *
+   * @returns {RegExp} - URL regular expression.
+   */
   private contructUrlRegex(): RegExp {
     const protocolRegexStrings = ['^(?<protocol>https?:\/\/)']; // Starts with http:// or https://
 
@@ -46,6 +60,13 @@ export class UrlValidator {
     return urlRegex;
   }
 
+  /**
+   * Extract a URL using a regular expression which validates it.
+   *
+   * @param {string} urlString - URL string to validate.
+   * @param {RegExp} urlRegex  - URL regular expression.
+   * @returns {boolean} - True if valid, false if not.
+   */
   private extractAndValidateUrl(urlString: string, urlRegex: RegExp): boolean {
     const regexGroups = urlString.match(urlRegex);
 
@@ -60,8 +81,14 @@ export class UrlValidator {
     return this.hasValidHostnameLength(regexGroups.groups.hostname);
   }
 
-  private hasValidHostnameLength(domainName: string): boolean {
-    if (domainName.length > 255) {
+  /**
+   * Checks if the hostname has a valid length.
+   *
+   * @param {string} hostname - Host name extracted from the URL string.
+   * @returns {boolean} - True if valid length, false if not.
+   */
+  private hasValidHostnameLength(hostname: string): boolean {
+    if (hostname.length > 255) {
       return false;
     }
 
